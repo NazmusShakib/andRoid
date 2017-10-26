63345ed0e33d7f3a8512876c6a449147aadf729b
Nazmus Shakib <mdshuvobc@gmail.com>
2017-10-26 14:48:43 +0600

    added facebook relative layout.

---
+++ app/src/main/java/com/example/gwe/practiceproject/RelativeLayoutFacebook.java
@@ -0,0 +1,13 @@
+package com.example.gwe.practiceproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelativeLayoutFacebook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout_facebook);
+    }
+}
